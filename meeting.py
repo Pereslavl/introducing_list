7be305bc78d694006a771f64096d2e7c61d15593
Sergey Kryukov <cydkom@gmail.com>
2018-12-28 00:47:08 +0300

    finish potential_attendees

--- meeting.py
+++ meeting.py
@@ -1,1 +1,6 @@
 attendees = ["Ken", "Alena", "Treasure"]
+attendees.append("Ashley")
+attendees.extend(["James", "Geil"])
+optional_invaitees = ["Ben J.", "Dave"]
+potential_attendees = attendees + optional_invaitees
+print("There are", len(potential_attendees), "potential attendees currently")
